8517f56b5b33993634f7fa8aee19f3b9f8f79565
grant <abernathy.grant@icloud.com>
2025-07-15 14:39:00 -0500

    App code

---
+++ src/game/core/app.cpp
@@ -0,0 +1,21 @@
+//
+// name:		app.cpp
+// description:	Basic application startup and shutdown
+//
+
+// file header
+#include "app.h"
+
+#include "system/param.h"
+#include "system/system.h"
+#include "system/controlMgr.h"
+#include "system/fileMgr.h"
+#include "profile/telemetry.h"
+
+#include "fwnet/netremotelog.h"
+#include "fwsys/timer.h"
+
+#include "system/param.h"
+#include "system/taskscheduler.h"
+#include "system/telemetry.h"
+#include "script/script.h"
